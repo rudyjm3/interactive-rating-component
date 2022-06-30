00993a510c85174f28df7911aa8b741ea7525b1a
rudyjm3 <rudyjm3@yahoo.com>
2022-06-30 07:52:15 -0400

    Added submit function
    Added an event listener click function to the submit button. This adds style to the .thankyou-container so it shows up. Time out function added within event listener to close thank you message after 5 sec and clears selected rating as well. Moved 2 variables from the ratingSelection() function to make them global. Removed commented out HTML

--- js/main.js
+++ js/main.js
@@ -9,19 +9,35 @@
 //          this.classList.add('selected');
 //       });
 //    };
+let outPut = document.getElementsByClassName('rating-output-container')[0];
+let rating = document.getElementsByClassName('rating-container')[0].children;
 
-function ratingSelection(ele) {
-
-   let rating = document.getElementsByClassName('rating-container')[0].children;
+function ratingSelection(ele) {  
    for(var i = 0; i < rating.length; i++) {
       console.log(rating);
-      console.log(rating[i]);
+      //console.log(rating[i]);
       rating[i].classList.remove('selected');
    } 
    ele.classList.add('selected');
    starRatingSelected = ele.innerHTML;
    console.log(ele.innerHTML + " was clicked");
    console.log(starRatingSelected);
-   document.getElementById('star-rating').innerHTML = "You selected " + starRatingSelected + " out of 5";
-}
+   outPut.innerHTML = "You selected " + starRatingSelected + " out of 5";
+};
+
+
+
+const submitBtn = document.getElementsByTagName('button');
+submitBtn[0].addEventListener('click', (button) => {
+   let thankYouPage = document.getElementsByClassName('thankyou-container')[0];
+   thankYouPage.style.cssText = 'opacity: 1; transform: scale(1);';
+   setTimeout(function(){
+      for(var i = 0; i < rating.length; i++) {
+         console.log(rating);
+         //console.log(rating[i]);
+         rating[i].classList.remove('selected');
+      } ;
+      thankYouPage.style.cssText = 'opacity: 1; transform: scale(0);'; 
+  }, 5000);//wait 5 seconds to close thank you message
+});
 
