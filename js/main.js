89c3625b71717a57b24f3bbc414ca1d7ff55f0e0
rudyjm3 <rudyjm3@yahoo.com>
2022-06-30 12:01:50 -0400

    Count down feature
    Added visual count down to the Thank you page. Used setInterval and clearInterval funtion to count down number from 5 to 0. Took few hours to do this because I tried to have it inside the setTimeout function and had issues, so moved it above timeout function. Fixed issue where when it reached zero and eslections cleared, if I made another selection which ran scrpit again the timer glitched and would show 0 inbetween each number as it counted down.

--- js/main.js
+++ js/main.js
@@ -29,15 +29,38 @@ function ratingSelection(ele) {
 
 const submitBtn = document.getElementsByTagName('button');
 submitBtn[0].addEventListener('click', (button) => {
+   let countNum = document.getElementById('countDwn');
    let thankYouPage = document.getElementsByClassName('thankyou-container')[0];
+
+   // let sec = 5;
+   // setInterval(function () {
+   //    countNum.innerHTML = sec;
+   //    sec--;
+   //    if (sec < 0) {
+   //       ++sec;
+   //    } 
+   // }, 1000);
+   let sec = 5;
+   let timer = setInterval(function(){
+      countNum.innerHTML = sec;
+      sec--;
+      if (sec < 0) {
+         clearInterval(timer);  
+      }
+   }, 1000);
+
    thankYouPage.style.cssText = 'opacity: 1; transform: scale(1);';
-   setTimeout(function(){
-      for(var i = 0; i < rating.length; i++) {
-         console.log(rating);
-         //console.log(rating[i]);
-         rating[i].classList.remove('selected');
-      } ;
-      thankYouPage.style.cssText = 'opacity: 1; transform: scale(0);'; 
-  }, 5000);//wait 5 seconds to close thank you message
+         setTimeout(function(){
+            for(var i = 0; i < rating.length; i++) {
+               console.log(rating);
+               //console.log(rating[i]);
+               rating[i].classList.remove('selected');
+               outPut.innerHTML = "";
+               countNum.innerHTML = "0";   
+            } ;
+            thankYouPage.style.cssText = 'opacity: 0; transform: scale(0);'; 
+      }, 6000);//wait 6 seconds to close thank you message
+      countNum.innerHTML = "5";
+
 });
 
